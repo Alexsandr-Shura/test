df2fc85791cb3bb16108ce0a6ac9e13124a42208
Alexsandr-Shura <pavlyuchenkov.alexsandr@yandex.ru>
2018-10-08 17:04:18 +0300

    )

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 class Hello{
     public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("A ");
-=======
->>>>>>> ad3dd2595888d86603ab80391d6ed1d654141326
         System.out.println("Hello World!");
 	System.out.println("B");
     }
